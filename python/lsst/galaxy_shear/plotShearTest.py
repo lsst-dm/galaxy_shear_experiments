c7c6c82d4f03456b5cce0dfc6fd58d711aef8bed
Perry Gee <pgee@physics.ucdavis.edu>
2015-10-19 14:32:58 -0700

    This changes plotShearTest so that it works with the organization where different subfields have different shear values. It is now assumed that the runs will be organized so that each subfield will have a single g1 and g2, which represents a single point on the e1 vs. applied g1 or e2 vs. applied g2 plot.  Any set of subfields which samples g1 or g2 in the range [-.05,+.05] should be enough to provide a plot.
    Also, I changed the plot routines to make it easier to compare plots side-by-side.

--- python/lsst/galaxy_shear/plotShearTest.py
+++ python/lsst/galaxy_shear/plotShearTest.py
@@ -21,7 +21,7 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import argparse
-import matplotlib.pyplot as plot
+import glob
 import math
 import sys
 import os.path
@@ -43,12 +43,13 @@ This program simply takes the values for a given filter and seeing, fits the poi
 to a line, and graphs the measured values and errors and the regression line.
 """
 
-def runPlot(useFilter, useSeeing, useIndex=None):
+def runPlot(analysis_file, useFilter, useSeeing, useIndex=None, display=1):
 
-    outSourceCat = afwTable.BaseCatalog.readFits(args.analysis_file)
+    # input file is the analyis of a set of subfields, all from the same measurement run
+    subfieldCat = afwTable.BaseCatalog.readFits(analysis_file)
 
     # Keys for fetching data from the output table
-    schema = outSourceCat.getSchema()
+    schema = subfieldCat.getSchema()
     filterKey = schema.find("filter").getKey()
     seeingKey = schema.find("seeing").getKey()
     shearKey = schema.find("shear_value").getKey()
@@ -62,6 +63,8 @@ def runPlot(useFilter, useSeeing, useIndex=None):
     countKey = schema.find("nsource").getKey()
     g1Key = schema.find("g1").getKey()
     g2Key = schema.find("g2").getKey()
+
+    # accumulate vectors of values for all the subfields in this run
     g1 = []
     g2 = []
     e = []
@@ -71,44 +74,97 @@ def runPlot(useFilter, useSeeing, useIndex=None):
     e2Avg = []
     e2Std = []
     nSources = 0
+    # Accumulate sums for likelikelihood calculation, which can then be used to get error
+    # for the slope and intercept of the 1D fit.  This is using the formulas in Numerical
+    # Recipes Chapter 15.2
+    s1 = 0
+    s1x = 0
+    s1y = 0
+    s1xx = 0
+    s1xy = 0
+    s2 = 0
+    s2x = 0
+    s2y = 0
+    s2xx = 0
+    s2xy = 0
+ 
     # Go through the catalog and get the values requested in the plot
-    for rec in outSourceCat:
+    for rec in subfieldCat:
         filter = rec.get(filterKey)
         seeing = rec.get(seeingKey)
-        if filter == useFilter and seeing == useSeeing:
-            g1.append(rec.get(g1Key))
-            g2.append(rec.get(g2Key))
+        if (useFilter is None or filter == useFilter) and (useSeeing is None or seeing == useSeeing):
+            x1 = rec.get(g1Key)
+            g1.append(x1)
+            x2 = rec.get(g2Key)
+            g2.append(x2)
             nSources = nSources + rec.get(countKey)
-            e1Avg.append(rec.get(e1AvgKey))
-            e1Std.append(rec.get(e1StdKey))
-            e2Avg.append(rec.get(e2AvgKey))
-            e2Std.append(rec.get(e2StdKey))
-            print "Stddev e1, e2: ", rec.get(e1StdKey), rec.get(e2StdKey)
-            #print rec.get(eAvgKey), rec.get(e1AvgKey), rec.get(e2AvgKey)
-            #print rec.get(eStdKey), rec.get(e1StdKey), rec.get(e2StdKey)
-    g1 = numpy.array(g1, dtype=float)
-    g2 = numpy.array(g2, dtype=float)
-    eStd = numpy.array(eStd, dtype=float)
-    e1Avg = numpy.array(e1Avg, dtype=float)
-    e1Std = numpy.array(e1Std, dtype=float)
-    e2Avg = numpy.array(e2Avg, dtype=float)
-    e2Std = numpy.array(e2Std, dtype=float)
+
+            # each subfield catalog contains and estimated mean and std error of the mean for e1 and e2
+            y1 = rec.get(e1AvgKey)
+            sigma1 = rec.get(e1StdKey)
+            e1Avg.append(y1)
+            e1Std.append(sigma1)
+            w1 = 1.0/(sigma1 * sigma1)
+            s1 = s1 + w1
+            s1x = s1x + x1 * w1
+            s1y = s1y + y1 * w1
+            s1xx = s1xx + x1 * x1 * w1
+            s1xy = s1xy + x1 * y1 * w1
+
+            y2 = rec.get(e2AvgKey)
+            sigma2 = rec.get(e2StdKey)
+            e2Avg.append(y2)
+            e2Std.append(sigma2)
+            w2 = 1.0/(sigma2 * sigma2)
+            s2 = s2 + w2
+            s2x = s2x + x2 * w2
+            s2y = s2y + y2 * w2
+            s2xx = s2xx + x2 * x2 * w2
+            s2xy = s2xy + x2 * y2 * w2
+
+    #  Now calculate the slope and intercept, as well as their errors
+    delta1 = (s1 * s1xx) - (s1x * s1x) 
+    b1 = (s1xx * s1y - s1x * s1xy)/delta1
+    m1 = (s1 * s1xy - s1x * s1y)/delta1
+    m1err = math.sqrt(s1/delta1)
+    b1err = math.sqrt(s1xx/delta1)
+
+    delta2 = (s2 * s2xx) - (s2x * s2x)
+    b2 = (s2xx * s2y - s2x * s2xy)/delta2
+    m2 = (s2 * s2xy - s2x * s2y)/delta2
+    m2err = math.sqrt(s2/delta2)
+    b2err = math.sqrt(s2xx/delta2)
+
+    nPoints = len(g1)
     
-    m1, b1 = numpy.polyfit(g1, e1Avg, 1)
-    m2, b2 = numpy.polyfit(g2, e2Avg, 1)
-    label = "%d gals, f%d_%.1f, e1m: %.4f b: %.4f) e2m: %.4f b:%.4f)"%(nSources, filter, seeing, m1, b1, m2, b2)
-    figure = plot.figure()
-    figure.suptitle(label)
-    ax = figure.add_subplot(111)
-    ax.set_xlabel("applied shear")
-    ax.set_ylabel("measured shear")
-    # plot the data with error bars
-    ax.errorbar(g1, e1Avg, yerr=e1Std, marker = "o", linestyle='None', label =label, color='red')
-    ax.errorbar(g2, e2Avg, yerr=e2Std, marker = "o", linestyle='None', label =label)
+    # print the results to the console
+    print "Fits for %s %d/%d sources, m1:%.6f+-%.6f, b1: %.6f+-%.6f m2:%.6f+-%.6f b2:%.6f+-%.6f"%(analysis_file,
+        nSources, nPoints, m1, m1err, b1, b1err, m2, m2err, b2, b2err)
     # plot a first order fit
-    ax.plot(g1, m1*g1 + b1)
-    ax.plot(g2, m2*g2 + b2)
-    plot.show()
+    if display:
+        import matplotlib.pyplot as plot
+        label = "Avg %.1f gals/subfield, m1=%.4f b1=%.4f, m2=%.4f b2=%.4f"%(float(nSources)/nPoints, m1, b1, m2, b2)
+        g1 = numpy.array(g1, dtype=float)
+        g2 = numpy.array(g2, dtype=float)
+        eStd = numpy.array(eStd, dtype=float)
+        e1Avg = numpy.array(e1Avg, dtype=float)
+        e1Std = numpy.array(e1Std, dtype=float)
+        e2Avg = numpy.array(e2Avg, dtype=float)
+        e2Std = numpy.array(e2Std, dtype=float)
+        zeros = numpy.zeros(nPoints, dtype=float)
+        figure = plot.figure()
+        figure.suptitle(label)
+        ax = figure.add_subplot(111)
+        ax.set_xlabel("applied shear")
+        ax.set_ylabel("measured shear")
+        # plot the data with error bars
+        ax.errorbar(g1, e1Avg, yerr=e1Std, marker = ".", linestyle='None', label =label, color='red')
+        ax.errorbar(g2, e2Avg, yerr=e2Std, marker = ".", linestyle='None', label =label, color="blue")
+        ax.plot(g1, m1*g1 + b1, color="black")
+        ax.plot(g2, m2*g2 + b2, color="cyan")
+        plot.show()
+
+    return nPoints, m1, m1err, b1, b1err, m2, m2err, b2, b2err
 
 if __name__ == "__main__":
 
@@ -119,9 +175,97 @@ if __name__ == "__main__":
 #   filter                plot only runs with the specified filter
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("analysis_file", type=str, help="Analysis fits file")
-    parser.add_argument("-f", "--filter", type=int, help="filter to be used for plotting", default=3)
-    parser.add_argument("-b", "--index", type=int, help="indexs of filter to be used for plotting", default=None)
-    parser.add_argument("-s", "--seeing", type=float, help="seeing to be used for plotting", default=0.5)
+    parser.add_argument("-a", "--analysis_file", type=str, help="Analysis fits file", default=None) 
+    parser.add_argument("-t", "----test", type=str, help="type of test: s, n, r", default=None)
+    parser.add_argument("-f", "--filter", type=int, help="filter to be used for plotting", default=None)
+    parser.add_argument("-b", "--index", type=int, help="index of filter to be used for plotting", default=None)
+    parser.add_argument("-s", "--seeing", type=float, help="seeing to be used for plotting", default=None)
+    parser.add_argument("-d", "--display", type=str, help="display the resulting plot", default=1)
     args = parser.parse_args()
-    runPlot(args.filter, args.seeing, useIndex=None)
+
+    #  If this is must a single analysis file, just display the fit
+    if args.test is None:
+        nPoints, m1, m1err, b1, b1err, m2, m2err, b2, b2err = runPlot(args.analysis_file, args.filter, args.seeing, useIndex=None, display=args.display)
+    
+    #  But if it is a test involving multiple runs, call runPlot multiple times, then display a graph of all the fits.
+    else:
+        if args.test == "s":
+            test_type = "stampsize" 
+        if args.test == "n":
+            test_type = "nGrowFootprint" 
+        if args.test == "r":
+            test_type = "nInitialRadii" 
+        files = glob.glob("subfield_anal_%s*.fits"%(args.test))
+        m1s = []
+        m2s = []
+        b1s = []
+        b2s = []
+        m1errs = []
+        m2errs = []
+        b1errs = []
+        b2errs = []
+        testnumbers = []
+        for file in files:
+            if args.display == 'all':
+                display = True
+            else:
+                display = False  
+            nPoints, m1, m1err, b1, b1err, m2, m2err, b2, b2err = runPlot(file, args.filter, args.seeing, useIndex=None, display=display)
+            m1s.append(m1)
+            m2s.append(m2)
+            b1s.append(b1)
+            b2s.append(b2)
+            m1errs.append(m1err)
+            m2errs.append(m2err)
+            b1errs.append(b1err)
+            b2errs.append(b2err)
+            start = len("subfield_anal_%s"%(args.test))
+            testnumbers.append(float(file[start:file.find('.fits')]))
+        if args.display:
+            import matplotlib.pyplot as plot
+            figure, (ax1, ax2, ax3, ax4) = plot.subplots(nrows=4, ncols=1)
+
+            figure.suptitle("Shear estimation bias vs. %s"%test_type)
+
+            ax1.set_title("m1", size="small") 
+            ax2.set_title("c1", size="small") 
+            ax3.set_title("m2", size="small") 
+            ax4.set_title("c2", size="small") 
+
+            testnumbers = numpy.array(testnumbers, dtype=float)
+            margin = (testnumbers.max() - testnumbers.min())/20.0
+
+            m1s = numpy.array(m1s, dtype=float)
+            m2s = numpy.array(m2s, dtype=float)
+            m1errs = numpy.array(m1errs, dtype=float)
+            m2errs = numpy.array(m2errs, dtype=float)
+            # put both graphs on the same units scale
+            yrange = 1.5 * max((m1s.max() - m1s.min() + m1errs.max(), m2s.max() - m2s.min() + m2errs.max())) 
+            # plot the data with error bars
+            ax1.set_xlim(testnumbers.min() - margin, testnumbers.max() + margin)
+            ax1.set_ylim((m1s.min() + m1s.max() - yrange)/2.0, (m1s.min() + m1s.max() + yrange)/2.0)
+            ax1.set_xticks([])
+            ax1.errorbar(testnumbers, m1s, yerr=m1errs, marker = ".", markersize=10, linestyle='None', color='red')
+
+
+            ax3.set_xlim(testnumbers.min() - margin, testnumbers.max() + margin)
+            ax3.set_ylim((m2s.min() + m2s.max() - yrange)/2.0, (m2s.min() + m2s.max() + yrange)/2.0)
+            ax3.set_xticks([])
+            ax3.errorbar(testnumbers, m2s, yerr=m2errs, marker = ".", markersize=10, linestyle='None')
+
+            # plot the data with error bars
+            b1s = numpy.array(b1s, dtype=float)
+            b2s = numpy.array(b2s, dtype=float)
+            b1errs = numpy.array(b1errs, dtype=float)
+            b2errs = numpy.array(b2errs, dtype=float)
+            # put both graphs on the same units scale
+            yrange = 1.5 * max((b1s.max() - b1s.min() + b1errs.max(), b2s.max() - b2s.min() + b2errs.max())) 
+            ax2.set_xlim(testnumbers.min() - margin, testnumbers.max() + margin)
+            ax2.set_ylim((b1s.min() + b1s.max() - yrange)/2.0, (b1s.min() + b1s.max() + yrange)/2.0)
+            ax2.set_xticks([])
+            ax2.errorbar(testnumbers, b1s, yerr=b1errs, marker = ".", markersize=10, linestyle='None', color='red')
+
+            ax4.set_xlim(testnumbers.min() - margin, testnumbers.max() + margin)
+            ax4.set_ylim((b2s.min() + b2s.max() - yrange)/2.0, (b2s.min() + b2s.max() + yrange)/2.0)
+            ax4.errorbar(testnumbers, b2s, yerr=b2errs, marker = ".", markersize=10, linestyle='None')
+            plot.show()
